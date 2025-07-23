ca0de6d2801668d3ace3e91da470e992c54c9760
Your Full Name <harshajami2005@gmail.com>
2025-07-23 18:07:39 +0530

    added recurring expenses,setremainder and budget alert

--- models/schema.js
+++ models/schema.js
@@ -12,7 +12,32 @@ const userschema = new mongoose.Schema({
     telegramid: { type: Number, required: true, unique: true },
     name: { type: String, required: true },
     username: { type: String},
-    expenses: [expensesschema]
+    expenses: [expensesschema],
+    recurringexpeses:[recurringschema],
+    remainders: [remainderschema]
 });
 
+const recurringschema=new mongoose.Schema({
+    amount: { type: Number, required: true },
+    category: { type: String, required: true },
+    date: { type: Date, default: Date.now },
+    frequency: { type: String, required: true } ,
+    duration: { type: Number, required: true }  ,
+    startdate: { type: Date, default: Date.now },
+    enddate: { type: Date}
+
+})
+const remainderschema = new mongoose.Schema({
+    title:{type:String,required:true},
+    description:{type:String,required:true},
+    amount:{type:Number,required:true},
+    date:{type:Date}
+    
+});
+const budgetalert=new mongoose.Schema({
+    category:{type:String,required:true},
+    limit:{type:Number,required:true},
+    spent:{type:Number,default:0},
+    date: { type: Date },
+})
 export const User = mongoose.model('User', userschema);
