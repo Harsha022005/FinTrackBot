6547b0939b91a9d8649cb0bcf3bcde891e69229d
Your Full Name <harshajami2005@gmail.com>
2025-08-04 15:09:06 +0530

    mongodb connetion error

--- models/db.js
+++ models/db.js
@@ -1,17 +1,16 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
-dotenv.config();
-const connectDB=async()=>{
-    try{
+import dotenv from "dotenv";
 
-       await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-       useUnifiedTopology: true,
-      });
-    }
-    catch (err){
-        console.error("Error connecting to MongoDB:", err);
-        process.exit(1); 
-    }
-}
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(" MongoDB connected successfully");
+  } catch (err) {
+    console.error(" Error connecting to MongoDB:", err.message);
+    process.exit(1);
+  }
+};
+
 export default connectDB;
